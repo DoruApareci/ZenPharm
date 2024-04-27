1c31b8e23eb20cb3c14cc0883782607adbbc5c2d
Vasile-Caspirovschi <vasilica.caspirovschi@gmail.com>
2024-04-27 23:26:02 +0300

    feat: add something

---
+++ ZenPharm.Web/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZenPharm.BL.Interfaces;
+using ZenPharm.Web.Models;
+
+namespace ZenPharm.Web.Controllers;
+
+public class ProductsController(IProductService productService) : Controller
+{
+    private readonly IProductService _productService = productService;
+
+    public IActionResult Products()
+    {
+        var products = _productService.GetAllProducts();
+        var productsViewModel = new ProductsViewModel();
+        productsViewModel.Products = products;
+        return View(productsViewModel);
+    }
+
+    public IActionResult ProductDetails(Guid productId)
+    {
+        var productViewModel = new ProductViewModel();
+        var product = _productService.GetProductById(productId);
+        productViewModel.Product = product;
+        return View(productViewModel);
+    }
+}
