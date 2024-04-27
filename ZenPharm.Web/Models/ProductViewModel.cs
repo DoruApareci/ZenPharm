29b175d2a23eb652fb82b49da57d0a546dafea98
Vasile-Caspirovschi <vasilica.caspirovschi@gmail.com>
2024-04-27 23:27:18 +0300

    feat: add products page and product details page

---
+++ ZenPharm.Web/Models/ProductViewModel.cs
@@ -0,0 +1,8 @@
+﻿using ZenPharm.DAL.Models;
+
+namespace ZenPharm.Web.Models;
+
+public class ProductViewModel
+{
+    public Product Product { get; set; }
+}
---
+++ ZenPharm.Web/Models/ProductsViewModel.cs
@@ -0,0 +1,8 @@
+﻿using ZenPharm.DAL.Models;
+
+namespace ZenPharm.Web.Models;
+
+public class ProductsViewModel
+{
+    public IEnumerable<Product> Products { get; set; } = [];
+}
