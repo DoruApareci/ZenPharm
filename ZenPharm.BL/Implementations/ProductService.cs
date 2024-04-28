9c2b5746870492783e226e0099d7c98c2f656955
DoruApareci <93883236+DoruApareci@users.noreply.github.com>
2024-04-28 11:26:28 +0300

    Extended ProdService

--- ZenPharm.BL/Implementations/ProductService.cs
+++ ZenPharm.BL/Implementations/ProductService.cs
@@ -8,10 +8,12 @@ namespace ZenPharm.BL.Implementations;
 public class ProductService : IProductService
 {
     private readonly ApplicationDbContext _context;
+    private readonly IImageService _imageService;
 
-    public ProductService(ApplicationDbContext context)
+    public ProductService(ApplicationDbContext context, IImageService imageService)
     {
         _context = context;
+        _imageService = imageService;
     }
 
     public IEnumerable<Product> GetAllProducts()
@@ -26,12 +28,23 @@ public class ProductService : IProductService
 
     public void AddProduct(Product product)
     {
+        var resp = _imageService.AddPhoto(product.FormFile);
+        product.Path = resp.Url.ToString();
+        product.PubId = resp.PublicId;
         _context.Products.Add(product);
         _context.SaveChanges();
     }
 
     public void UpdateProduct(Product product)
     {
+        var toUpdate = GetProductById(product.Id);
+        if (product.FormFile != null)
+        {
+            _imageService.DeletePhoto(toUpdate.PubId);
+            var resp = _imageService.AddPhoto(product.FormFile);
+            product.Path = resp.Url.ToString();
+            product.PubId = resp.PublicId;
+        }
         _context.Entry(product).State = EntityState.Modified;
         _context.SaveChanges();
     }
@@ -44,5 +57,11 @@ public class ProductService : IProductService
             _context.Products.Remove(product);
             _context.SaveChanges();
         }
+    }
+
+    public IEnumerable<Product> GetProducts(int page, int count)
+    {
+        var prods = _context.Products.Skip((page-1)*count).Take(count);
+        return prods;
     }
 }
--- ZenPharm.BL/Interfaces/IProductService.cs
+++ ZenPharm.BL/Interfaces/IProductService.cs
@@ -5,6 +5,7 @@ namespace ZenPharm.BL.Interfaces;
 public interface IProductService
 {
     IEnumerable<Product> GetAllProducts();
+    IEnumerable<Product> GetProducts(int page, int count);
     Product GetProductById(Guid id);
     void AddProduct(Product product);
     void UpdateProduct(Product product);
