b50a5824048a5db37b188c749d61218064258e75
DoruApareci <93883236+DoruApareci@users.noreply.github.com>
2024-04-28 11:25:41 +0300

    dashboard styles, js and libs

---
+++ ZenPharm.Web/wwwroot/js/admin/site.js
@@ -0,0 +1,13 @@
+﻿(function ($) {
+    "use strict";
+    // Sidebar Toggler
+    $('.sidebar-toggler').click(function () {
+        $('.sidebar, .content').toggleClass("open");
+        return false;
+    });
+
+    //-----------------------------------------
+    //template goes from here
+    //-----------------------------------------
+
+})(jQuery);
