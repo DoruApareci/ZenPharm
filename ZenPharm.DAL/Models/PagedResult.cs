1cfec4faa17f1ce99452cbe2212f4d6d326dfbef
DoruApareci <93883236+DoruApareci@users.noreply.github.com>
2024-04-30 03:22:50 +0300

    Add PagedResult class to ZenPharm.DAL.Models namespace

---
+++ ZenPharm.DAL/Models/PagedResult.cs
@@ -0,0 +1,21 @@
+﻿namespace ZenPharm.DAL.Models;
+
+public class PagedResult<T>
+{
+    public PagedResult(IEnumerable<T> value, int totalPages, int currentPage, int totalItems, int pageSize = 10)
+    {
+        Value = value;
+        TotalPages = totalPages;
+        CurrentPage = currentPage;
+        PageSize = pageSize;
+        TotalItems = totalItems;
+    }
+
+    public IEnumerable<T> Value { get; }
+    public int TotalPages { get; }
+    public int CurrentPage { get; }
+    public int PageSize { get; }
+    public int TotalItems { get; }
+
+
+}
